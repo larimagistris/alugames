cf26390cf6f7beefdf4b6faa8dc717d7fb60c45e
Lari Magistris <larimagisitris@hotmail.com>
2025-12-03 15:35:25 -0300

    adiciona confirmacao para devolucao do jogo

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@ function alterarStatus(id) {
 
     let imagem = botaoClicado.querySelector(".dashboard__item__img");
     let botao = botaoClicado.querySelector(".dashboard__item__button");
+    let nomeDoJogo = botaoClicado.querySelector(".dashboard__item__name")
 
     let estaAlugado = imagem.classList.contains("dashboard__item__img--rented");
 
     if (estaAlugado) {
-        imagem.classList.remove("dashboard__item__img--rented");
-        botao.classList.remove("dashboard__item__button--return");
-        botao.innerHTML = "Alugar";
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeDoJogo.innerHTML}?`)) {
+            imagem.classList.remove("dashboard__item__img--rented");
+            botao.classList.remove("dashboard__item__button--return");
+            botao.innerHTML = "Alugar";
+        }
     } else {
         imagem.classList.add("dashboard__item__img--rented");
         botao.classList.add("dashboard__item__button--return");
